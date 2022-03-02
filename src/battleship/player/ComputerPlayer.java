9f0ff2d74ca59672402f672c2f64898200d0ad7e
edwinboersema <edwinboersema@gmail.com>
2022-03-02 15:27:17 +0100

    added body to play()

--- src/battleship/player/ComputerPlayer.java
+++ src/battleship/player/ComputerPlayer.java
@@ -1,5 +1,10 @@
 package battleship.player;
 
+import battleship.ships.Coordinate;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class ComputerPlayer extends Player{
 
     public ComputerPlayer() {
@@ -7,7 +12,16 @@ public class ComputerPlayer extends Player{
     }
 
     @Override
-    public void play() {
+    public Coordinate play() {
+        Supplier<Integer> r = () -> new Random().nextInt(1, 11);
 
+        while(true) {
+            Coordinate randomCoordinate = new Coordinate(r.get(), r.get());
+            // validate that coordinate is valid
+            if (isEmpty(randomCoordinate)) {
+                // return coordinate
+                return randomCoordinate;
+            }
+        }
     }
 }
