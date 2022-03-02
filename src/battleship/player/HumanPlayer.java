f35e3fbbb4d6b00f3829d60f20e6ec23a3923b3c
edwinboersema <edwinboersema@gmail.com>
2022-03-02 15:54:58 +0100

    added human player input functionality

--- src/battleship/player/HumanPlayer.java
+++ src/battleship/player/HumanPlayer.java
@@ -1,5 +1,8 @@
 package battleship.player;
 
+import battleship.IO.IOUtil;
+import battleship.ships.Coordinate;
+
 public class HumanPlayer extends Player{
 
     public HumanPlayer(String name) {
@@ -7,7 +10,35 @@ public class HumanPlayer extends Player{
     }
 
     @Override
-    public void play() {
+    public Coordinate play() {
+        String characterArray = "ABCDEFGHJI";
 
+        // get valid coordinate
+        while (true) {
+            String input = IOUtil.askInput("Type in a coordinate to fire at (e.g. \"C4\"): ");
+            // parse 2nd character to int
+            try {
+                String letter = input.substring(0,1);
+                int y = Integer.parseInt(input.substring(1,2));
+                // check if input matches expected format
+                if (letter.matches("[a-iA-I]") &&
+                        (y > 0 && y <= 10)) {
+                    // convert input into coordinate
+                    int x = characterArray.indexOf(letter);
+                    Coordinate shot = new Coordinate(x, y);
+                    if (shots.contains(shot)) {
+                        // if coordinate has been tried already, notify user
+                        IOUtil.printExclamation("That coordinate has been tried already, guess again.");
+                    } else {
+                        return shot;
+                    }
+                } else {
+                    // if not, notify player and get input again
+                    System.out.println("Invalid input, a valid input would be: \"E3\"");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, a valid input would be: \"E3\"");
+            }
+        }
     }
 }
