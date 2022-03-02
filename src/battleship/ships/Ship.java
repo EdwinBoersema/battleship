4ad9b34d392eed58e99ca0b247d82a98053df837
edwinboersema <edwinboersema@gmail.com>
2022-03-02 13:16:34 +0100

    added ascii functions

--- src/battleship/ships/Ship.java
+++ src/battleship/ships/Ship.java
@@ -6,11 +6,16 @@ import java.util.List;
 public abstract class Ship {
     private final String NAME;
     private final int SIZE;
+    private final String horizontalAscii;
+    private final String verticalAscii;
+    private boolean horizontalAlignment;
     private List<Coordinate> coordinates = new ArrayList<>();
 
-    protected Ship(String name, int length) {
+    protected Ship(String name, int length, String horizontalAscii, String verticalAscii) {
         this.NAME = name;
         this.SIZE = length;
+        this.horizontalAscii = horizontalAscii;
+        this.verticalAscii = verticalAscii;
     }
 
     public boolean isHit(Coordinate coordinate) {
@@ -18,6 +23,18 @@ public abstract class Ship {
     }
 
     // todo: give coordinates, and checkCoordinates function
+
+    public String getAscii(int index) {
+        if (horizontalAlignment) {
+            return String.valueOf(horizontalAscii.toCharArray()[index]);
+        } else {
+            return String.valueOf(verticalAscii.toCharArray()[index]);
+        }
+    }
+
+    public void setHorizontalAlignment(boolean alignment) {
+        this.horizontalAlignment = alignment;
+    }
 
     public int getSize() {
         return SIZE;
@@ -34,8 +51,5 @@ public abstract class Ship {
     public void setCoordinates(List<Coordinate> coordinates) {
         this.coordinates = coordinates;
     }
-
     public abstract boolean isSunk();
-
-
 }
